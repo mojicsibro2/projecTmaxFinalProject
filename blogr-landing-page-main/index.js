168afca30ffa8f9c30db913a5bf70851ad186382
ABDULLAHI OMOWUMI IBRAHEEM <71957195+mojicsibro2@users.noreply.github.com>
2023-02-10 23:57:21 -0800

    Add files via upload

---
+++ blogr-landing-page-main/index.js
@@ -0,0 +1,19 @@
+
+function myFunc1(){
+    var menu = document.querySelector('.burgerMenu');
+    menu.style.display = "none";
+    var menu = document.querySelector('.navContainer');
+    menu.style.display = "block";
+    var menu = document.querySelector('.close');
+    menu.style.display = "block";
+};
+
+function myfunc2() {
+    var menu = document.querySelector('.close');
+    menu.style.display = 'none';
+    var menu = document.querySelector('.burgerMenu');
+    menu.style.display = "block";
+    var menu = document.querySelector('.navContainer');
+    menu.style.display = "none";
+
+}
